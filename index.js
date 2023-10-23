b78846e36deff2c1b5a25e59bc360faed1455787
Brad DeFauw <badefauw@gmail.com>
2023-10-23 14:40:01 -0500

    Build a Node class

---
+++ index.js
@@ -0,0 +1,9 @@
+// create a node class for the binary search tree
+class Node {
+    // constructor to initialize a new node
+    constructor(data){
+        this.data = data; // data to be stored in the node
+        this.left = null; // reference to the left child node
+        this.right = null; // reference to the right child node
+    }
+}
