a4665a5122616c766f75890a072a05f30f5c6397
Brad DeFauw <badefauw@gmail.com>
2023-10-25 09:22:19 -0500

    visualized binary search tree with prettyPrint() function

--- index.js
+++ index.js
@@ -42,7 +42,21 @@ class Tree {
     }
 }
 
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
 // Example usage:
 const dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(dataArray);
-console.log(tree.root); // Output: The root node of the balanced binary search tree
+// call the prettyPrint function with the root of the tree
+prettyPrint(tree.root)
